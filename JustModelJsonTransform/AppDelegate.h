db33e1937ec3edbc33e4314403a8d70c576e1926
Just <JustLee.me@hotmail.com>
2017-05-19 21:30:47 +0800

    Initial Commit

---
+++ JustModelJsonTransform/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JustModelJsonTransform
+//
+//  Created by Just on 2017/5/19.
+//  Copyright © 2017年 Just. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
